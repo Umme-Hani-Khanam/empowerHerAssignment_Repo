3cbe619ceeff192d4ff4402ae8557721a208e5a7
Umme-Hani-Khanam <khanamummehani0@gmail.com>
2025-11-23 18:56:59 +0530

    Multilevel Destructuring

---
+++ js_assignment_/index3.js
@@ -0,0 +1,22 @@
+const people = [
+  {
+    name: "Alice",
+    address: {
+      city: "New York",
+      street: { name: "Broadway", number: 123 }
+    }
+  },
+  {
+    name: "Bob",
+    address: {
+      city: "Los Angeles",
+      street: { name: "Sunset Boulevard", number: 456 }
+    }
+  }
+];
+
+const result = people.map(({name:nameofperson, address: {city, street: { name: streetName } } }) =>
+  `${nameofperson} lives in ${city} on ${streetName}`
+);
+
+console.log(result);
