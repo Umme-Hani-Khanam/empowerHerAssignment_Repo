7bb330568647593c1c04a653bfb8e652e07e4885
Umme-Hani-Khanam <khanamummehani0@gmail.com>
2025-11-24 23:39:02 +0530

    16th question

---
+++ ES+_feature_assignment_/index6.js
@@ -0,0 +1,7 @@
+const isEven = (n) => n % 2 === 0;
+let marks=30;
+result = marks >= 35 ? "Pass" : "Fail";
+const greet = (name) => 
+  console.log(`Hello, ${name ? name : "Guest"}`);
+greet("hani");
+
