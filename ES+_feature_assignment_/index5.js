2f32032d010ca91515bb0beeb3ada129bfe80aba
Umme-Hani-Khanam <khanamummehani0@gmail.com>
2025-11-24 23:33:55 +0530

    15th question

---
+++ ES+_feature_assignment_/index5.js
@@ -0,0 +1,15 @@
+let username = "Hani";
+let course = "JavaScript";
+
+console.log(`Hello ${username}, welcome to the ${course} course!`);
+const name = "Sam";
+const age = 21;
+
+const student = {
+  name,
+  age,
+  greet() {
+    console.log("Hello");
+  }
+};
+const getFullName = (first, last) => `${first} ${last}`;
