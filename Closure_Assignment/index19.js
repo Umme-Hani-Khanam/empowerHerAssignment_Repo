5a4cc5364df7285c0dabb97f9d4e44b2cd38ac3c
Umme-Hani-Khanam <khanamummehani0@gmail.com>
2025-11-26 00:32:28 +0530

    19th Question of Closure

---
+++ Closure_Assignment/index19.js
@@ -0,0 +1,12 @@
+let age=20;//global variable
+function displayAge()
+{//function execution context
+  console.log(age);
+}
+displayAge();//function call
+
+function changeAge(){//updates age
+  age=age+2;
+  console.log(age);//print updated age
+}
+changeAge();
